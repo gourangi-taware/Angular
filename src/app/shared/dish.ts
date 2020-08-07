39eb5a03a6e9f545d08aab2f4081bdb5bb39b6ce
gourangi-taware <gourangi0309@gmail.com>
2020-08-08 01:53:35 +0530

    Data Binding

--- src/app/shared/dish.ts
+++ src/app/shared/dish.ts
@@ -1,3 +1,5 @@
+import { Comment } from './comment';
+
 export class Dish{
     id:string;
     name:string;
@@ -7,11 +9,5 @@ export class Dish{
     label:string;
     price:string;
     description:string;
-   
-}
-export class Comments{
-   rating:number;
-   comment:string;
-   author:string;
-   date:string;
+    comments:Comment[];
 }
